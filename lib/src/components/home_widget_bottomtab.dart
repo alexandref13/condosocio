48bd3f7e64f6f61f66aefd6a4ca270602769b01e
alexandref13 <alexandrefilhoalvo@gmail.com>
2021-04-24 20:24:25 -0300

    pagina convites

--- lib/src/components/home_widget_bottomtab.dart
+++ lib/src/components/home_widget_bottomtab.dart
@@ -169,7 +169,7 @@ class _HomeBottomTabState extends State<HomeBottomTab> {
                                 Padding(
                                   padding: const EdgeInsets.only(bottom: 10),
                                   child: Icon(
-                                    FontAwesome.tv,
+                                    AntDesign.adduser,
                                     size: 35,
                                     color: Colors.white,
                                   ),
