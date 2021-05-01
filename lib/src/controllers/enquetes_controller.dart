9e2be6469d7771aa7dbda1e4b231e5d4cbcdf259
alexandref13 <alexandrefilhoalvo@gmail.com>
2021-05-01 15:05:30 -0300

    pagina enquetes

--- lib/src/controllers/enquetes_controller.dart
+++ lib/src/controllers/enquetes_controller.dart
@@ -1,17 +1,33 @@
 import 'package:get/get.dart';
 
 class EnquetesController extends GetxController {
-  var votos = 1.obs;
+  var votos = 0.obs;
   var perguntaA = 0.obs;
   var perguntaB = 0.obs;
+  var isSelected = false.obs;
+  var date = DateTime.now().obs;
 
   addPerguntaA() {
     perguntaA.value++;
     votos.value++;
+    date.value = DateTime(
+      date.value.year,
+      date.value.month,
+      date.value.day,
+      date.value.hour,
+      date.value.minute,
+    );
+    isSelected(true);
   }
 
   addPerguntaB() {
     perguntaB.value++;
     votos.value++;
+    date.value = DateTime(
+      date.value.year,
+      date.value.month,
+      date.value.day,
+    );
+    isSelected(true);
   }
 }
