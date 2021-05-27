516297b1c822eef73f813b936f5c639f0e2264c5
alexandref13 <alexandrefilhoalvo@gmail.com>
2021-05-27 16:43:55 -0300

    att

--- lib/src/components/visualizar_ouvidoria/lista_visualizar_ouvidoria.dart
+++ lib/src/components/visualizar_ouvidoria/lista_visualizar_ouvidoria.dart
@@ -1,6 +1,5 @@
 import 'package:condosocio/src/controllers/ouvidoria/visualizar_ouvidoria_controller.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_icons/flutter_icons.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
--- lib/src/pages/ouvidoria/visualizar_ouvidoria.dart
+++ lib/src/pages/ouvidoria/visualizar_ouvidoria.dart
@@ -4,6 +4,7 @@ import 'package:condosocio/src/controllers/ouvidoria/visualizar_ouvidoria_contro
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:condosocio/src/components/visualizar_ouvidoria/lista_visualizar_ouvidoria.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class VisualizarOuvidoria extends StatefulWidget {
   @override
@@ -26,6 +27,48 @@ class _VisualizarOuvidoriaState extends State<VisualizarOuvidoria> {
                     context,
                     visualizarOuvidoria.search.value,
                     visualizarOuvidoria.onSearchTextChanged,
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    color: Theme.of(context).accentColor,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          child: Text(
+                            'DATA',
+                            style: GoogleFonts.montserrat(
+                              fontSize: 16.0,
+                              color: Theme.of(context)
+                                  .textSelectionTheme
+                                  .selectionColor,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          child: Text(
+                            'HORA',
+                            style: GoogleFonts.montserrat(
+                              fontSize: 16.0,
+                              color: Theme.of(context)
+                                  .textSelectionTheme
+                                  .selectionColor,
+                            ),
+                          ),
+                        ),
+                        Text(
+                          'TÍTULO',
+                          style: GoogleFonts.montserrat(
+                            fontSize: 16.0,
+                            color: Theme.of(context)
+                                .textSelectionTheme
+                                .selectionColor,
+                          ),
+                        ),
+                        Container(),
+                        Container(),
+                      ],
+                    ),
                   ),
                   Expanded(child: listaVisualizarOuvidoria())
                 ],
