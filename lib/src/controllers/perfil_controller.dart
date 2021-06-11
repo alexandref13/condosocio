d0c22d67de9f320e31fedbfbad8e06399dc82e0f
alexandref13 <alexandrefilhoalvo@gmail.com>
2021-06-10 21:59:38 -0300

    aaa

--- lib/src/controllers/perfil_controller.dart
+++ lib/src/controllers/perfil_controller.dart
@@ -60,7 +60,6 @@ class PerfilController extends GetxController {
 
       /*phone.value.text = cellMaskFormatter.getUnmaskedText();
       var date = birthdate.value.text.split('/');*/
-   
 
       var date = birthdate.value.text.split('/');
       newDate.value = '${date[2]}-${date[1]}-${date[0]}';
@@ -69,8 +68,8 @@ class PerfilController extends GetxController {
       var dados = json.decode(response.body);
 
       isLoading(false);
-      
-       loginController.birthdate.value = '${date[2]}/${date[1]}/${date[0]}';
+
+      loginController.birthdate.value = '${date[2]}/${date[1]}/${date[0]}';
       return dados;
     }
   }
@@ -81,9 +80,7 @@ class PerfilController extends GetxController {
     secondName.value.text = loginController.sobrenome.value;
     var date = loginController.birthdate.value.replaceAll('-', '/').split('/');
     birthdate.value.text = '${date[2]}/${date[1]}/${date[0]}';
-    gender.value.text = loginController.genero.value;
-   
-
+    itemSelecionado.value = loginController.genero.value;
   }
 
   @override
