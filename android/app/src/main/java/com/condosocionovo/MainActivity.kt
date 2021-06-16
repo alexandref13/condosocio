205685dbee76da457c416334e628176a9cb0f3ca
Alexandrealvo <alexandrebacana@gmail.com>
2021-06-16 03:40:58 -0300

    configs de version 2

--- android/app/src/main/java/com/condosocionovo/MainActivity.kt
+++ android/app/src/main/java/com/condosocionovo/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.condosocio
+package com.condosocionovo
 import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
