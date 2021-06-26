3726e24384916e1f7b1743f636ef18bd08270209
Alexandrealvo <alexandrebacana@gmail.com>
2021-06-26 11:20:15 -0300

    confirm delete

--- lib/src/pages/reservas/detalhes_reservas.dart
+++ lib/src/pages/reservas/detalhes_reservas.dart
@@ -10,6 +10,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class DetalhesReservas extends StatelessWidget {
@@ -23,6 +24,58 @@ class DetalhesReservas extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    confirmDelete(String text, VoidCallback function) {
+      Alert(
+        image: Icon(
+          Icons.highlight_off,
+          color: Colors.yellowAccent,
+          size: 60,
+        ),
+        style: AlertStyle(
+          backgroundColor: Theme.of(context).textSelectionTheme.selectionColor,
+          animationType: AnimationType.fromTop,
+          isCloseButton: false,
+          isOverlayTapDismiss: false,
+          //descStyle: GoogleFonts.poppins(color: Colors.red,),
+          animationDuration: Duration(milliseconds: 300),
+          titleStyle: GoogleFonts.poppins(
+            color: Theme.of(context).errorColor,
+            fontSize: 18,
+          ),
+        ),
+        context: context,
+        title: text,
+        buttons: [
+          DialogButton(
+            child: Text(
+              "Cancelar",
+              style: GoogleFonts.montserrat(
+                color: Theme.of(context).textSelectionTheme.selectionColor,
+                fontSize: 16,
+              ),
+            ),
+            onPressed: () {
+              Get.back();
+            },
+            width: 80,
+            color: Theme.of(context).errorColor,
+          ),
+          DialogButton(
+            child: Text(
+              "OK",
+              style: GoogleFonts.montserrat(
+                color: Theme.of(context).textSelectionTheme.selectionColor,
+                fontSize: 16,
+              ),
+            ),
+            onPressed: function,
+            width: 80,
+            color: Theme.of(context).primaryColor,
+          )
+        ],
+      ).show();
+    }
+
     var day = DateTime.parse(detalhesReservasController.data.value);
     var newDate = DateFormat.yMMMMd('pt').format(day);
 
@@ -310,7 +363,12 @@ class DetalhesReservas extends StatelessWidget {
                                 onPressed: () {
                                   if (addReservasController.isChecked.value ==
                                       true) {
-                                    detalhesReservasController
+
+                             confirmDelete(
+                                      'Deseja realmente deletar o evento ${detalhesReservasController.titulo.value}',
+                                      () {
+                                                 
+                                                            detalhesReservasController
                                         .deleteReserva()
                                         .then(
                                       (value) {
@@ -329,6 +387,9 @@ class DetalhesReservas extends StatelessWidget {
                                         }
                                       },
                                     );
+                                                },
+                                    );
+
                                   }
                                 },
                                 child: Text(
